929da25fcc2b858fc3ba36e85c7e7380366a4f5b
Ben Cornelis & Drew <drew@finstrom.us>
2015-06-02 11:35:14 -0700

    add tracking of users and dropdwon interface to switch between bank account holders

--- js/script.js
+++ js/script.js
@@ -17,11 +17,17 @@ BankAccount.prototype.withdraw = function(amount) {
 
 var refreshBalance = function(account) {
   $("#balance-display").text("$" + account.balance)
+  clearInputs();
 };
+
+var clearInputs = function() {
+  $("input").val("");
+}
 
 //jQuery
 $( document ).ready(function() {
   "use strict";
+
     console.log( "jQuery Ready" );
 
     $("#jqtest").text("jQuery Ready");
@@ -33,9 +39,21 @@ $( document ).ready(function() {
       var initialDeposit = parseInt($("input#initialDeposit").val());
 
       var newAccount = new BankAccount(accountHolder, initialDeposit);
-      accounts.push(newAccount);
+      var accountId = accounts.push(newAccount) - 1;
+
+      var optionHTML = "<option value='" + accountId + "'>" + newAccount.accountHolder + "</option>";
+      $("select#login-accountHolder").append(optionHTML);
+      $("select#login-accountHolder").val(accountId);
+      var select = $("select#login-accountHolder")
 
       refreshBalance(newAccount);
+      $("#login-panel").show();
+    });
+
+    $("select#login-accountHolder").change(function () {
+      var id = parseInt($("select#login-accountHolder").val());
+      var account = accounts[id];
+      refreshBalance(account);
     });
 
     $("form#update-account").submit(function(event) {
@@ -43,7 +61,10 @@ $( document ).ready(function() {
 
       var depositAmount = parseInt($("input#new-deposit").val());
       var withdrawalAmount = parseInt($("input#new-withdrawal").val());
-      var account = accounts[accounts.length - 1];
+
+      var id = parseInt($("select#login-accountHolder").val());
+      var account = accounts[id];
+
       if (depositAmount) { account.deposit(depositAmount); }
       if (withdrawalAmount) { account.withdraw(withdrawalAmount); }
       refreshBalance(account);
